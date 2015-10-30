32a97e61de38d0a707511bdb6e66f5b751c7f673
Yassine Lamgarchal <yassine.lamgarchal@enovance.com>
2015-10-30 11:45:41 +0100

    Lay the base to use click for CLI parsing
    This change lay the base code in order to implement a click based
    command line parsing.
    
    With this library for command line parsing it would make it easy to
    logically separate code for each DCI endpoints, each one living their
    lives in their own class.
    
    Expected CLI examples are :
    
      * dcictl component-list
      * dcictl component-show --id XXXX
      * dcictl component-update --id XXXX --etag YYYY
    
    Each component will inherit from the DCIBaseComponent object, resulting
    in components ready to roll almost immediately
    
    Change-Id: I185c965e6e50c7aec298cc858721bec15fc1de41
    Co-Authored-By: Yanis Guenane <yguenane@redhat.com>

--- dciclient/v1/exceptions.py
+++ dciclient/v1/exceptions.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 #
 # Copyright 2015 Red Hat, Inc.
@@ -15,10 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from dciclient.v1 import shell_commands
 
-def main():
-    shell_commands.cli()
-
-if __name__ == '__main__':
-    main()
+class ClientError(Exception):
+    """DCI client error."""
