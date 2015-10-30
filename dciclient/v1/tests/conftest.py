c49942fb01986a89bd36d98cb6ad62de603ee99c
Yassine Lamgarchal <yassine.lamgarchal@enovance.com>
2015-10-30 11:45:41 +0100

    Lay the base to use click for CLI parsing
    This change lay the base code in order to implement a click based
    command line parsing.
    
    With this library for command line parsing it would make it easy to
    logically separate code for each DCI endpoints, each one living their
    lives in their own class.
    
    Expected CLI examples are :
    
      * dcictl component-list
      * dcictl component-show --id XXXX
      * dcictl component-update --id XXXX --etag YYYY
    
    Each component will inherit from the DCIBaseComponent object, resulting
    in components ready to roll almost immediately
    
    Change-Id: I185c965e6e50c7aec298cc858721bec15fc1de41
    Co-Authored-By: Yanis Guenane <yguenane@redhat.com>

--- dciclient/v1/tests/conftest.py
+++ dciclient/v1/tests/conftest.py
@@ -16,9 +16,21 @@
 
 from dci.server.tests import conftest as server_conftest
 from dciclient import v1 as dci_client
+from dciclient.v1 import dciclientlib as dci_clientlib
+from dciclient.v1.shell_commands import _get_http_session
+from dciclient.v1.shell_commands import cli
 from dciclient.v1.tests import utils
 
+import click
 import pytest
+
+
+@pytest.fixture(autouse=True, scope='module')
+def remove_decorators():
+    noop = lambda f: f
+    cli.command = lambda *args, **kwargs: noop
+    click.option = lambda *args, **kwargs: noop
+    click.pass_obj = noop
 
 
 @pytest.fixture(scope='session')
@@ -49,4 +61,23 @@ def client(server, db_provisioning):
     )
     flask_adapter = utils.FlaskHTTPAdapter(server.test_client())
     client.s.mount('http://dci_server.com', flask_adapter)
+    return client
+
+
+@pytest.fixture
+def http_session(server, db_provisioning):
+    session = _get_http_session('http://dci_server.com', 'admin', 'admin')
+    flask_adapter = utils.FlaskHTTPAdapter(server.test_client())
+    session.mount('http://dci_server.com', flask_adapter)
+    return session
+
+
+@pytest.fixture
+def client_v1(server, db_provisioning):
+    client = dci_clientlib.DCIClient(
+        end_point='http://dci_server.com/api/v1',
+        login='admin', password='admin'
+    )
+    flask_adapter = utils.FlaskHTTPAdapter(server.test_client())
+    client._s.mount('http://dci_server.com/api/v1', flask_adapter)
     return client
