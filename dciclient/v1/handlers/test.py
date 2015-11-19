5f14e3a3e06eca397b767bac01317fc8cfc81685
Yanis Guenane <yguenane@redhat.com>
2015-11-19 14:57:36 +0100

    Integrate test endpoint CRUD operations
    This change aims to bring various command to interact with the
    test endpoint in the DCI server. The commands are :
    
      * dcictl test-list
      * dcictl test-show
      * dcictl test-update
      * dcictl test-create
      * dcictl test-delete
    
    Change-Id: Ibd46ec2bc6afedc98f0d9ba24fb94a6f8fdecec7

---
+++ dciclient/v1/handlers/test.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from dciclient.v1.handlers import dcibaseresource
+
+
+class Test(dcibaseresource.DCIBaseResource):
+    ENDPOINT_URI = 'tests'
+
+    def __init__(self, session):
+        super(Test, self).__init__(session, self.ENDPOINT_URI)
+
+    def create(self, name):
+        return super(Test, self).create(name=name)
+
+    def update(self, id, etag, name):
+        return super(Test, self).update(id=id, etag=etag, name=name)
+
+    def delete(self, id, etag):
+        return super(Test, self).delete(id=id, etag=etag)
+
+    def show(self, id):
+        return super(Test, self).show(id=id)
