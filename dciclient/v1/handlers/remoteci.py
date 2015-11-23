09af3dd844ef3c300ee7d6541399dbd9b5cf82ad
Yanis Guenane <yguenane@redhat.com>
2015-11-23 10:49:19 +0100

    Integrate team endpoint CRUD operations
    This change aims to bring various command to interact with the team
    endpoint in the DCI server.
    
    The commands are :
    
      * dcictl team-list
      * dcictl team-show
      * dcictl team-update
      * dcictl team-create
      * dcictl team-delete
    
    Change-Id: I544ce54b838f7bb109443dbf0ec511e5a28d8890

---
+++ dciclient/v1/handlers/remoteci.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from dciclient.v1.handlers import dcibaseresource
+
+
+class RemoteCI(dcibaseresource.DCIBaseResource):
+    ENDPOINT_URI = 'remotecis'
+
+    def __init__(self, session):
+        super(RemoteCI, self).__init__(session, self.ENDPOINT_URI)
+
+    def create(self, name, team_id, data):
+        return super(RemoteCI, self).create(name=name, team_id=team_id,
+                                            data=data)
+
+    def update(self, id, etag, name, team_id=None, data=None):
+        return super(RemoteCI, self).update(id=id, etag=etag, name=name,
+                                            team_id=team_id, data=data)
+
+    def delete(self, id, etag):
+        return super(RemoteCI, self).delete(id=id, etag=etag)
+
+    def show(self, id):
+        return super(RemoteCI, self).show(id=id)
--- dciclient/v1/tests/conftest.py
+++ dciclient/v1/tests/conftest.py
@@ -19,6 +19,7 @@ from dciclient import v1 as dci_client
 from dciclient.v1.shell_commands import _get_http_session
 from dciclient.v1.shell_commands import cli
 from dciclient.v1.shell_commands import componenttype
+from dciclient.v1.shell_commands import remoteci
 from dciclient.v1.shell_commands import team
 from dciclient.v1.shell_commands import test
 from dciclient.v1.tests import utils
@@ -36,6 +37,7 @@ def remove_decorators():
     click.pass_obj = noop
 
     imp.reload(componenttype)
+    imp.reload(remoteci)
     imp.reload(team)
     imp.reload(test)
 
