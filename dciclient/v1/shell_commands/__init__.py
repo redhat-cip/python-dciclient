05925afc35fda87603ce1edcb925e699667781a9
Yanis Guenane <yguenane@redhat.com>
2015-11-25 17:56:10 +0100

    Integrate jobstate endpoint read-only operations
    This change aims to bring various command to interact with the jobstate endpoint
    in the DCI server. The commands are :
    
      * dcictl jobstate-list
      * dcictl jobstate-show
    
    Change-Id: I18fc2e1a4ba9b81b6f274cdc596719c1f8cbec82

--- dciclient/v1/shell_commands/__init__.py
+++ dciclient/v1/shell_commands/__init__.py
@@ -43,6 +43,7 @@ def cli(ctx, dci_login, dci_password, dci_cs_url):
 
 import dciclient.v1.shell_commands.componenttype  # noqa
 import dciclient.v1.shell_commands.job  # noqa
+import dciclient.v1.shell_commands.jobstate  # noqa
 import dciclient.v1.shell_commands.remoteci  # noqa
 import dciclient.v1.shell_commands.team  # noqa
 import dciclient.v1.shell_commands.test  # noqa
