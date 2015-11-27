c5b13c775adb0ebd8dbebd1edf7b26801df5ef39
Yanis Guenane <yguenane@redhat.com>
2015-11-27 10:27:54 +0100

    Integrate component endpoint CRUD operations
    This change aims to bring various command to interact with the component
    endpoint in the DCI server. The commands are :
    
      * dcictl component-list
      * dcictl component-show
      * dcictl component-create
      * dcictl component-delete
    
    Change-Id: I7a5e93787f760a19e35abe239e8b45c7521a1d0a

--- dciclient/v1/shell_commands/__init__.py
+++ dciclient/v1/shell_commands/__init__.py
@@ -41,6 +41,7 @@ def _get_http_session(dci_cs_url, login, password):
 def cli(ctx, dci_login, dci_password, dci_cs_url):
     ctx.obj = _get_http_session(dci_cs_url, dci_login, dci_password)
 
+import dciclient.v1.shell_commands.component  # noqa
 import dciclient.v1.shell_commands.componenttype  # noqa
 import dciclient.v1.shell_commands.jobdefinition  # noqa
 import dciclient.v1.shell_commands.remoteci  # noqa
