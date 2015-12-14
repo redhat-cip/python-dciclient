1b25a834cb5ba669e5323e1ed8358faace5c0e8b
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2015-12-14 16:04:44 +0100

    Add commodity function 'get_full_data' on jobs.
    This function will retrieve the datas of the tests, remotecis
    and components associated to a given job.
    
    This will be used by the agents.
    
    Change-Id: Id17c95918c4ad61763fc2150e48513906e08d40a

--- dciclient/v1/handlers/job.py
+++ dciclient/v1/handlers/job.py
@@ -15,6 +15,7 @@
 # under the License.
 
 from dciclient.v1.handlers import dcibaseresource
+from dciclient.v1.handlers import jobdefinition
 
 
 class Job(dcibaseresource.DCIBaseResource):
@@ -24,6 +25,7 @@ class Job(dcibaseresource.DCIBaseResource):
 
     def __init__(self, dci_client):
         super(Job, self).__init__(dci_client, self.ENDPOINT_URI)
+        self._dci_client = dci_client
 
     def create(self, recheck, remoteci_id, team_id, jobdefinition_id=None):
         return super(Job, self).create(recheck=recheck,
@@ -31,5 +33,29 @@ class Job(dcibaseresource.DCIBaseResource):
                                        team_id=team_id,
                                        jobdefinition_id=jobdefinition_id)
 
+    def schedule(self, remoteci_id):
+        data_json = {'remoteci_id': remoteci_id}
+        return self._s.post('%s/schedule' % self._end_point_with_uri,
+                            json=data_json)
+
     def get(self, id, where=None, embed=None):
         return super(Job, self).get(id=id, where=where, embed=embed)
+
+    def get_full_data(self, id):
+        # Get the job with embed on test and remoteci
+        embed = 'jobdefinition,jobdefinition.test,remoteci'
+        job = self.get(id=id, embed=embed).json()['job']
+        # Get the components of the jobdefinition
+        l_jobdefinition = jobdefinition.JobDefinition(self._dci_client)
+        jobdefinition_components = l_jobdefinition.get_components(
+            job['jobdefinition']['id']).json()['components']
+
+        # Aggregate the data of each resource
+        full_data = {'remoteci': job['remoteci']['data'],
+                     'test': job['jobdefinition']['test']['data'],
+                     'components': []}
+
+        for component in jobdefinition_components:
+            full_data['components'].append(component['data'])
+
+        return full_data
