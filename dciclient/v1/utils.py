2804fc3c20e86abb7e03a5f7c0498dd835b397e0
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2015-12-24 18:12:57 +0100

    Add run_command utility.
    Change-Id: Ic1155a65abfd84d2e731c2088e7b914631e5df80

--- dciclient/v1/utils.py
+++ dciclient/v1/utils.py
@@ -14,10 +14,16 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from prettytable import PrettyTable
+from dciclient.v1 import api
+
+import fcntl
+import os
+import select
+import subprocess
 
 import click
 import json
+import prettytable
 import six
 
 
@@ -40,7 +46,7 @@ def print_json(result_json):
 def print_prettytable(data, headers):
     DEFAULT_HEADERS = ['Property', 'Value']
     headers = headers or DEFAULT_HEADERS
-    table = PrettyTable(headers)
+    table = prettytable.PrettyTable(headers)
 
     if headers == DEFAULT_HEADERS:
         for item in sorted(six.iteritems(data)):
@@ -75,3 +81,36 @@ def format_output(output, format, item=None, headers=None):
     else:
         to_display = output[item] if item else output
         print_prettytable(to_display, headers)
+
+
+def run_command(context, cmd, cwd, jobstate_id):
+    """This function execute a command and send its log which will be
+    attached to a jobstate.
+    """
+    output = six.StringIO.StringIO()
+    pipe_process = subprocess.Popen(cmd, cwd=cwd,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
+
+    fcntl.fcntl(pipe_process.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
+    inputs = [pipe_process.stdout]
+    outputs = []
+
+    while True:
+        readable, writable, exceptional = select.select(inputs, outputs,
+                                                        inputs, 1)
+        if not readable:
+            break
+        read = pipe_process.stdout.read().decode('UTF-8', 'ignore')
+        if len(read) == 0:
+            break
+        print(read)
+        output.write(read)
+
+    # equivalent to pipe_process.wait(), avoid deadlock, see Popen doc.
+    pipe_process.communicate()
+
+    api.file.create(context, name='_'.join(cmd), content=output.getvalue(),
+                    mime='text/plain', jobstate_id=jobstate_id)
+    output.close()
+    return pipe_process.returncode
