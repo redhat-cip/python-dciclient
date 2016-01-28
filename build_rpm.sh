35b88174aee02eab587ef03d02b2bd5e4d61e8ae
Yanis Guenane <yguenane@redhat.com>
2016-01-28 10:00:02 +0100

    agent/feeder: Create a github feeder and an tox agent
    This commits create :
    
      * a github feeder that will just create a new component and
        jobdefinition based on the github url
    
      * an tox agent that will simply run tox and upload
        the result to dci
    
    By having this in the code base it makes it easy to be able to ensure a
    basic functionement of the whole application.
    
    Change-Id: I9bc8719b0bb3130358f9b681f6289568a50400aa

--- build_rpm.sh
+++ build_rpm.sh
@@ -9,18 +9,16 @@ SHA=$(git rev-parse HEAD | cut -c1-8)
 rm -rf ${HOME}/rpmbuild
 rpmdev-setuptree
 cp ${PROJ_NAME}.spec ${HOME}/rpmbuild/SPECS/
-git archive master --format=tgz --output=${HOME}/rpmbuild/SOURCES/${PROJ_NAME}-0.0.${DATE}git${SHA}.tgz
+git archive HEAD --format=tgz --output=${HOME}/rpmbuild/SOURCES/${PROJ_NAME}-0.0.${DATE}git${SHA}.tgz
 sed -i "s/VERS/${DATE}git${SHA}/g" ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
 rpmbuild -bs ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
 
 # Build the RPMs in a clean chroot environment with mock to detect missing
 # BuildRequires lines.
-for arch in fedora-22-x86_64 fedora-23-x86_64 epel-7-x86_64; do
+for arch in fedora-23-x86_64 epel-7-x86_64; do
 
     if [[ "$arch" == "fedora-23-x86_64" ]]; then
         RPATH='fedora/23/x86_64'
-    elif [[ "$arch" == "fedora-22-x86_64" ]]; then
-        RPATH='fedora/22/x86_64'
     else
         RPATH='el/7/x86_64'
     fi
@@ -41,5 +39,5 @@ for arch in fedora-22-x86_64 fedora-23-x86_64 epel-7-x86_64; do
     sed -i '$ienabled=1' ${HOME}/.mock/${arch}-with-dci-repo.cfg
 
     mkdir -p development
-    mock -r $arch rebuild --resultdir=development/${RPATH} ${HOME}/rpmbuild/SRPMS/${PROJ_NAME}*
+    mock -r ${HOME}/.mock/${arch}-with-dci-repo.cfg rebuild --resultdir=development/${RPATH} ${HOME}/rpmbuild/SRPMS/${PROJ_NAME}*
 done
