ab99558d9f08597ea088912aded3e9bc6f262733
Yanis Guenane <yguenane@redhat.com>
2016-01-28 10:00:02 +0100

    agent/feeder: Create a github feeder and an helloworld agent
    This commits create :
    
      * a github feeder that will just create a new component and
        jobdefinition based on the github url
    
      * an helloworld agent that will simply echo helloword and upload
        the result to dci
    
    By having this in the code base it makes it easy to be able to ensure a
    basic functionement of the whole application.
    
    Change-Id: I9bc8719b0bb3130358f9b681f6289568a50400aa

---
+++ dciclient/v1/run_commands.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from dciclient.v1.api import file
+from dciclient.v1.api import jobstate
+
+import fcntl
+import os
+import select
+import subprocess
+
+import six
+import sys
+
+
+def run_command(context, cmd, cwd, jobstate_id, team_id):
+    """This function execute a command and send its log which will be
+    attached to a jobstate.
+    """
+    output = six.StringIO()
+    print('* Processing command: %s' % cmd)
+    print('* Working directory: %s' % cwd)
+    pipe_process = subprocess.Popen(cmd, cwd=cwd,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT)
+
+    fcntl.fcntl(pipe_process.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
+    inputs = [pipe_process.stdout]
+    outputs = []
+
+    while True:
+        readable, writable, exceptional = select.select(inputs, outputs,
+                                                        inputs, 5)
+        if not readable:
+            break
+        pstdout = pipe_process.stdout.read().decode('UTF-8', 'ignore')
+        if len(pstdout) == 0:
+            break
+        else:
+            print(pstdout)
+            output.write(pstdout)
+
+    pipe_process.wait()
+
+    file.create(context, name='_'.join(cmd), content=output.getvalue(),
+                mime='text/plain', jobstate_id=jobstate_id)
+    output.close()
+    return pipe_process.returncode
+
+
+def run_commands(context, cmds, cwd, jobstate_id, job_id, team_id):
+    for cmd in cmds:
+        if isinstance(cmd, dict):
+            rc = run_command(context, cmd['cmd'], cmd['cwd'], jobstate_id,
+                             team_id)
+        else:
+            rc = run_command(context, cmd, cwd, jobstate_id, team_id)
+
+        if rc != 0:
+            error_msg = "Failed on command %s" % cmd
+            jobstate.create(context, "failure", error_msg, job_id, team_id)
+            print(error_msg)
+            sys.exit(1)
