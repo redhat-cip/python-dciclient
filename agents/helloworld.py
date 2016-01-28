408b53d94e3d715d11e06a4f4b979b373f8dd158
Yanis Guenane <yguenane@redhat.com>
2016-01-28 10:00:02 +0100

    agent/feeder: Create a dummy feeder and an helloworld agent
    This commits create :
    
      * a dummy feeder that will just create a new component and
        jobdefinition
    
      * an helloworld agent that will simply echo helloword and upload
        the result to dci
    
    By having this in the code base it makes it easy to be able to ensure a
    basic functionement of the whole application.
    
    Change-Id: I9bc8719b0bb3130358f9b681f6289568a50400aa

---
+++ agents/helloworld.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from dciclient.v1.api import context
+from dciclient.v1.api import job
+from dciclient.v1.api import jobstate
+from dciclient.v1 import run_commands
+
+
+import sys
+
+
+def get_pre_run_commands():
+
+    commands = [
+        ['echo', 'pre-run']
+    ]
+
+    return commands
+
+
+def get_running_commands():
+
+    commands = [
+        ['echo', 'Helloworld']
+    ]
+
+    return commands
+
+
+def retrieve_jobinformation(dci_context, remoteci_id):
+    """Retrieve job information"""
+    scheduled_job = job.schedule(dci_context, remoteci_id)
+    if scheduled_job.status_code != 201:
+        print("No job scheduled: '%s'" % scheduled_job.json())
+        sys.exit(0)
+
+    scheduled_job_id = scheduled_job.json()['job']['id']
+    print("* Job scheduled: %s" % scheduled_job_id)
+    full_data = job.get_full_data(dci_context, scheduled_job_id)
+
+    return scheduled_job_id, full_data
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("%s remoteci_id team_id" % sys.argv[0])
+        sys.exit(1)
+    remoteci_id = sys.argv[1]
+    team_id = sys.argv[2]
+
+    dci_context = context.build_dci_context()
+    new_job_id, jobinformation = retrieve_jobinformation(dci_context,
+                                                         remoteci_id)
+
+    # 1. Create the pre-run state
+    pre_run_state = jobstate.create(dci_context,
+                                    'pre-run',
+                                    'Running pre-run commands',
+                                    new_job_id) .json()
+    pre_run_state_id = pre_run_state['jobstate']['id']
+
+    # 2. Install requirements packages and tools.
+    pre_run_commands = get_pre_run_commands()
+    run_commands.run_commands(dci_context, pre_run_commands, '/var/tmp',
+                              pre_run_state_id, new_job_id, team_id)
+
+    # 5. Create the running state
+    running_state = jobstate.create(dci_context, 'running',
+                                    'Running Helloworld',
+                                    new_job_id).json()
+    running_state_id = running_state['jobstate']['id']
+
+    # 6. Run the echo helloworld command
+    running_commands = get_running_commands()
+    run_commands.run_commands(dci_context, running_commands, '/var/tmp',
+                              running_state_id, new_job_id, team_id)
+
+    # If we are there, then all the commands have succeed, create success state
+    jobstate.create(dci_context, 'success', 'Helloworld run successfully',
+                    new_job_id)
+
+if __name__ == '__main__':
+    main()
