6b5cc685d19b3cdf54ce0da15391193372fda068
Gonéri Le Bouder <goneri@redhat.com>
2016-05-30 15:58:20 -0400

    user the User-Agent to trace the lib version
    Put the string 'python-dciclient_XXX' in the User-Agent header to
    be able to trace requests coming from outdated client.
    
    Change-Id: I76b867861e238b4e3bc90bd765ecc75f897304d5

---
+++ dciclient/v1/tests/test_lib.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from dciclient.v1.api import job
+from dciclient import version
+
+import mock
+
+
+def test_standard_headers(job_id, dci_context):
+    with mock.patch('requests.sessions.Session.send'):
+        job.get(dci_context, job_id)
+        # the request that will be should be send
+        prepared_request = dci_context.session.send.call_args[0]
+        ua = 'python-dciclient_' + version.__version__
+        assert prepared_request[0].headers['User-Agent'] == ua
