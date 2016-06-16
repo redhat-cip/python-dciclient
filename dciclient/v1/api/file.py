fe526be183101492697e46fbfd69739d7ca04bc2
Gonéri Le Bouder <goneri@redhat.com>
2016-06-16 16:11:11 -0400

    helper: reduce the number of mandatory parameters
    Use sensible default value as much as possible to simplify the API.
    
    Change-Id: I865b5da74c68cae8e2248ccf18a1aa61e790ef37

--- dciclient/v1/api/file.py
+++ dciclient/v1/api/file.py
@@ -23,7 +23,7 @@ TABLE_HEADERS = ['id', 'name', 'content', 'mime', 'md5', 'jobstate_id',
                  'team_id', 'created_at']
 
 
-def create(context, name, content, mime, jobstate_id=None, md5=None,
+def create(context, name, content, mime='text/plain', jobstate_id=None, md5=None,
            job_id=None):
     headers = {'DCI-NAME': name,
                'DCI-MIME': mime,
