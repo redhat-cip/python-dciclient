27ff7851c1e9c936ca50f814cf5d8ebf3bbd7947
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2016-06-30 15:12:39 +0200

    job.create(): add components list.
    Change-Id: I08dd241af0bd0ac4ff42b64be57c17ee0b815710

--- dciclient/v1/api/job.py
+++ dciclient/v1/api/job.py
@@ -25,10 +25,12 @@ TABLE_HEADERS = ['id', 'status', 'recheck', 'jobdefinition_id', 'remoteci_id',
                  'team_id', 'etag', 'created_at', 'updated_at']
 
 
-def create(context, recheck, remoteci_id, team_id, jobdefinition_id=None):
+def create(context, recheck, remoteci_id, team_id, jobdefinition_id=None,
+           components=None):
     job = base.create(context, RESOURCE, recheck=recheck,
                       remoteci_id=remoteci_id, team_id=team_id,
-                      jobdefinition_id=jobdefinition_id)
+                      jobdefinition_id=jobdefinition_id,
+                      components=components)
     context.last_job_id = job.json()['job']['id']
     return job
 
