68e4d69fc1a4ca22b15508c4c726854a57afcc28
Gonéri Le Bouder <goneri@redhat.com>
2016-11-21 18:07:40 -0500

    cli: limit file-list to just on job
    Before this commig `file-list` was returning the whole list of files.
    This list already huge and likely to increase quickly.
    With this patch we know only list the files related to one job.
    
    Change-Id: Idec1b99fde93be2f34878e481c319957f94a592d

--- dciclient/v1/tests/shell_commands/test_file.py
+++ dciclient/v1/tests/shell_commands/test_file.py
@@ -15,8 +15,21 @@
 # under the License.
 
 from __future__ import unicode_literals
+import json
 
 
 def test_show(runner, file_id):
     result = runner.invoke(['file-show', file_id])
     assert 'testsuite errors' in result.output
+
+
+def test_list(runner, job_id):
+    result = runner.invoke(['file-list', '--job-id', job_id])
+    files = json.loads(result.output)['files']
+    assert len(files) == 1
+    assert files[0]['name'] == 'res_junit.xml'
+
+
+def test_list_without_job_id(runner):
+    result = runner.invoke(['file-list'])
+    assert 'Error: Missing option "--job-id".' in result.output
