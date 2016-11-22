534bc7cdac128d68353475b2ededf612d4f7cd42
Yanis Guenane <yguenane@redhat.com>
2016-11-22 14:51:05 +0100

    Add component-status feature
    Change-Id: I6c59931ab9aeecc984ad91be0bdfa550029cea15

--- dciclient/v1/shell_commands/component.py
+++ dciclient/v1/shell_commands/component.py
@@ -118,6 +118,26 @@ def show(context, id):
                         component.TABLE_HEADERS)
 
 
+@cli.command("component-status",
+             help="Show an overview of the last jobs associated to this component type")  # noqa
+@click.option("--type", required=True)
+@click.option("--topic_id", required=True)
+@click.pass_obj
+def status(context, type, topic_id):
+    """status(context, type, topic_id)
+
+    Show current status of a component
+
+    >>> dcictl component-status [OPTIONS]
+
+    :param string type: Type of component to look up status for
+    :param string topic_id: The topic ID for the list of components to return
+    """
+    result = component.status(context, type=type, topic_id=topic_id)
+    utils.format_output(result, context.format, 'jobs',
+                        component.TABLE_STATUS_HEADERS)
+
+
 @cli.command("component-file-upload", help="Attach a file to a component.")
 @click.argument("id")
 @click.option("--path", required=True)
