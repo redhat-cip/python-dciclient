3bcabfe0490c58d6df3bd51e4f214bac924b2ddf
Gonéri Le Bouder <goneri@redhat.com>
2016-11-24 10:22:45 -0500

    code clean up
    - simplify the logic print_prettytable()
    - runner.invoke returns a Python structure. This simplify the
      unit-test
      a lot since this is what we expect more of the cases.
      This also allow us to properly handle a JSON decode failure. We
    know
      print the JSON string that we failed to decode and the exit code.
    - runner.invoke_raw returns the original raw Result object.
    - improve test_prettytable_output code coverage and simplify the
      tests.
    - test: properly get the user team_id
    
    Change-Id: I340e24458effe5aa6a4af7220a4131af1b1b9def

--- dciclient/v1/tests/shell_commands/test_component.py
+++ dciclient/v1/tests/shell_commands/test_component.py
@@ -15,82 +15,33 @@
 # under the License.
 
 from __future__ import unicode_literals
-import json
 
 
-def test_prettytable_output(runner):
-    topic = runner.invoke(['topic-create', '--name', 'osp'])
-    topic = json.loads(topic.output)['topic']
-    assert topic['name'] == 'osp'
-
-    result = runner.invoke(['component-create', '--name', 'foo',
-                            '--type', 'bar', '--export_control',
-                            '--topic_id', topic['id']])
-    component = json.loads(result.output)['component']
-
-    result = runner.invoke(['--format', 'table', 'component-show',
-                            component['id']])
-
-    output = result.output.split('\n')
-    # NOTE(spredzy) : The expected output for a table format looks like the
-    #                 following :
-    #
-    # +------------
-    # |  id | name
-    # +------------
-    # | id1 | name1
-    # | id2 | name2
-    # | id3 | name3
-    # +------------
-    #
-    # The header variable below represents the header data, when more than one
-    # space is located between the string and the '|' space number is shrink to
-    # one ( this is what ' '.join(string.split()) does
-    #
-    # The data variable below represents the actual data, when more than one
-    # space is located between the string and the '|' space number is shrink to
-    # one ( this is what ' '.join(string.split()) does
-    header = ' '.join(output[1].split())
-    data = ' '.join(output[3].split())
-
-    expected_data = (component['id'],
-                     component['name'],
-                     component['canonical_project_name'],
-                     component['type'],
-                     component['title'],
-                     component['message'],
-                     component['url'],
-                     component['created_at'],
-                     component['export_control'],
-                     component['updated_at'])
-
-    assert header == ('| id | name | canonical_project_name '
-                      '| type | title | message | url '
-                      '| data | created_at | export_control '
-                      '| updated_at |')
-
-    assert data == ('| %s | %s | %s | %s | %s | %s | %s | {} '
-                    '| %s | %s | %s |' % expected_data)
+def test_prettytable_output(runner, topic_id):
+    component = runner.invoke_raw_parse([
+        'component-create',
+        '--name', 'osp',
+        '--type', 'repo',
+        '--topic_id', topic_id])
+    assert component['name'] == 'osp'
+    assert component == runner.invoke_raw_parse([
+        'component-show', component['id']])
 
 
 def test_list(runner):
-    topic = runner.invoke(['topic-create', '--name', 'osp'])
-    topic = json.loads(topic.output)['topic']
-
-    result = runner.invoke(['team-list'])
-    teams = json.loads(result.output)['teams']
+    topic = runner.invoke(['topic-create', '--name', 'osp'])['topic']
+    teams = runner.invoke(['team-list'])['teams']
     team_id = teams[0]['id']
 
-    topic_team = runner.invoke(['topic-attach-team', topic['id'],
-                                '--team_id', team_id])
-    topic_team = json.loads(topic_team.output)
+    runner.invoke(['topic-attach-team', topic['id'],
+                   '--team_id', team_id])
 
     runner.invoke(['component-create', '--name', 'foo', '--type', 'bar',
                    '--topic_id', topic['id']])
     runner.invoke(['component-create', '--name', 'bar', '--type', 'bar2',
                    '--topic_id', topic['id']])
-    result = runner.invoke(['component-list', '--topic_id', topic['id']])
-    components = json.loads(result.output)['components']
+    components = runner.invoke([
+        'component-list', '--topic_id', topic['id']])['components']
 
     assert len(components) == 2
     assert components[0]['name'] == 'foo'
@@ -98,123 +49,105 @@ def test_list(runner):
 
 
 def test_create(runner):
-    topic = runner.invoke(['topic-create', '--name', 'osp'])
-    topic = json.loads(topic.output)['topic']
+    topic = runner.invoke(['topic-create', '--name', 'osp'])['topic']
 
     component = runner.invoke(['component-create', '--name', 'foo',
-                               '--type', 'foobar', '--topic_id', topic['id']])
-    component = json.loads(component.output)['component']
+                               '--type', 'foobar', '--topic_id',
+                               topic['id']])['component']
     assert component['name'] == 'foo'
 
 
 def test_delete(runner):
-    topic = runner.invoke(['topic-create', '--name', 'osp'])
-    topic = json.loads(topic.output)['topic']
-
-    result = runner.invoke(['component-create', '--name', 'foo',
-                            '--type', 'bar', '--topic_id', topic['id']])
-    component = json.loads(result.output)['component']
+    topic = runner.invoke(['topic-create', '--name', 'osp'])['topic']
+    component = runner.invoke(['component-create', '--name', 'foo',
+                               '--type', 'bar', '--topic_id',
+                               topic['id']])['component']
 
     result = runner.invoke(['component-delete', component['id']])
-    result = json.loads(result.output)
 
     assert result['message'] == 'Component deleted.'
 
 
 def test_show(runner):
-    topic = runner.invoke(['topic-create', '--name', 'osp'])
-    topic = json.loads(topic.output)['topic']
+    topic = runner.invoke(['topic-create', '--name', 'osp'])['topic']
     assert topic['name'] == 'osp'
 
-    result = runner.invoke(['component-create', '--name', 'foo',
-                            '--type', 'bar', '--export_control',
-                            '--topic_id', topic['id']])
-    component = json.loads(result.output)['component']
+    component = runner.invoke([
+        'component-create', '--name', 'foo',
+        '--type', 'bar', '--export_control',
+        '--topic_id', topic['id']])['component']
 
     result = runner.invoke(['component-show', component['id']])
-    component = json.loads(result.output)['component']
 
-    assert component['name'] == 'foo'
+    assert result['component']['name'] == 'foo'
 
 
 def test_file_support(runner, tmpdir):
     td = tmpdir
     p = td.join("hello.txt")
     p.write("content")
-    topic = runner.invoke(['topic-create', '--name', 'osp'])
-    topic = json.loads(topic.output)['topic']
+    topic = runner.invoke(['topic-create', '--name', 'osp'])['topic']
 
     component = runner.invoke(['component-create', '--name', 'foo',
                                '--type', 'foobar', '--topic_id', topic['id'],
-                               '--export_control'])
-    component = json.loads(component.output)['component']
+                               '--export_control'])['component']
 
     # upload
-    result = runner.invoke(['component-file-upload', component['id'],
-                            '--path', p.strpath])
-    new_f = json.loads(result.output)['component_file']
+    new_f = runner.invoke(['component-file-upload', component['id'],
+                           '--path', p.strpath])['component_file']
     assert new_f['size'] == 7
 
     # show
-    result = runner.invoke(['component-file-show', component['id'],
-                            '--file_id', new_f['id']])
-    new_f = json.loads(result.output)['component_file']
+    new_f = runner.invoke(['component-file-show', component['id'],
+                           '--file_id', new_f['id']])['component_file']
     assert new_f['size'] == 7
 
     # download
-    result = runner.invoke(['component-file-download', component['id'],
-                            '--file_id', new_f['id'],
-                            '--target', td.strpath + '/my_file'])
+    runner.invoke_raw(['component-file-download', component['id'],
+                       '--file_id', new_f['id'],
+                       '--target', td.strpath + '/my_file'])
     assert open(td.strpath + '/my_file', 'r').read() == 'content'
 
     # list
-    result = runner.invoke(['component-file-list', component['id']])
-    my_list = json.loads(result.output)['component_files']
+    my_list = runner.invoke(['component-file-list',
+                             component['id']])['component_files']
     assert len(my_list) == 1
     assert my_list[0]['size'] == 7
 
     # delete
-    result = runner.invoke(['component-file-delete', component['id'],
-                            '--file_id', new_f['id']])
+    runner.invoke_raw([
+        'component-file-delete', component['id'],
+        '--file_id', new_f['id']])
     result = runner.invoke(['component-file-show', component['id'],
                             '--file_id', new_f['id']])
-    assert json.loads(result.output)['status_code'] == 404
+    assert result['status_code'] == 404
 
 
 def test_enable_export_control(runner):
-    topic = runner.invoke(['topic-create', '--name', 'osp'])
-    topic = json.loads(topic.output)['topic']
+    topic = runner.invoke(['topic-create', '--name', 'osp'])['topic']
 
-    result = runner.invoke(['component-create', '--name', 'foo',
-                            '--type', 'bar', '--topic_id', topic['id']])
-    component = json.loads(result.output)['component']
+    component = runner.invoke([
+        'component-create', '--name', 'foo',
+        '--type', 'bar', '--topic_id',
+        topic['id']])['component']
 
     result = runner.invoke(['component-update', component['id'],
                             '--export-control'])
-
-    result = json.loads(result.output)
-
     assert result['message'] == 'Export Control Enabled.'
 
 
 def test_disable_export_control(runner):
-    topic = runner.invoke(['topic-create', '--name', 'osp'])
-    topic = json.loads(topic.output)['topic']
+    topic = runner.invoke(['topic-create', '--name', 'osp'])['topic']
 
-    result = runner.invoke(['component-create', '--name', 'foo',
-                            '--type', 'bar', '--topic_id', topic['id']])
-    component = json.loads(result.output)['component']
+    component = runner.invoke([
+        'component-create', '--name', 'foo',
+        '--type', 'bar', '--topic_id',
+        topic['id']])['component']
 
     result = runner.invoke(['component-update', component['id'],
                             '--export-control'])
-
-    result = json.loads(result.output)
-
     assert result['message'] == 'Export Control Enabled.'
 
     result = runner.invoke(['component-update', component['id'],
                             '--no-export-control'])
-
-    result = json.loads(result.output)
-
     assert result['message'] == 'Export Control Disabled.'
