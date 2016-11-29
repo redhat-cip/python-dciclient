3c8f8af0a3d5e251fd7fb620a248f1a882c17fef
François Charlier <francois.charlier@redhat.com>
2016-11-29 21:36:25 +0100

    [WIP] client implementation of token authentication
    Change-Id: I88b290303f502c657941c2941bf7fd09e4aa8151

--- dciclient/v1/api/context.py
+++ dciclient/v1/api/context.py
@@ -12,33 +12,32 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import datetime
 import os
 import sys
 
 import requests
 from requests.adapters import HTTPAdapter
+from requests.auth import AuthBase
+from requests.compat import urlparse
 from requests.packages.urllib3.util.retry import Retry
 
+from dciclient.v1 import auth
 from dciclient import version
 
 
-class DciContext(object):
+class DciContextBase(object):
     API_VERSION = 'api/v1'
 
-    def __init__(self, dci_cs_url, login, password, max_retries=0,
-                 user_agent=None):
-        self.session = self._build_http_session(login, password, user_agent)
-        retries = Retry(total=max_retries,
-                        backoff_factor=0.1)
-        self.login = login
-        self.session.mount('http://', HTTPAdapter(max_retries=retries))
-        self.session.mount('https://', HTTPAdapter(max_retries=retries))
+    def __init__(self, dci_cs_url, max_retries=0, user_agent=None):
+        super(DciContextBase, self).__init__()
+        self.session = self._build_http_session(user_agent, max_retries)
         self.dci_cs_api = '%s/%s' % (dci_cs_url, DciContext.API_VERSION)
         self.last_jobstate_id = None
         self.last_job_id = None
 
-    @staticmethod
-    def _build_http_session(login, password, user_agent):
+    @classmethod
+    def _build_http_session(self, user_agent, max_retries):
         session = requests.Session()
         session.headers.setdefault('Content-Type', 'application/json')
         if not user_agent:
@@ -47,12 +46,24 @@ class DciContext(object):
         session.headers['Client-Version'] = (
             'python-dciclient_%s' % version.__version__
         )
-        session.auth = (login, password)
+        retries = Retry(total=max_retries,
+                        backoff_factor=0.1)
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+
         return session
 
 
+class DciContext(DciContextBase):
+    def __init__(self, dci_cs_url, login, password, max_retries=0,
+                 user_agent=None):
+        super(DciContext, self).__init__(dci_cs_url, max_retries, user_agent)
+        self.login = login
+        self.session.auth = (login, password)
+
+
 def build_dci_context(dci_cs_url=None, dci_login=None, dci_password=None,
-                      user_agent=None, max_retries=80):
+                      dci_api_secret=None, user_agent=None, max_retries=80):
     dci_cs_url = dci_cs_url or os.environ.get('DCI_CS_URL', '')
     dci_login = dci_login or os.environ.get('DCI_LOGIN', '')
     dci_password = dci_password or os.environ.get('DCI_PASSWORD', '')
@@ -64,3 +75,60 @@ def build_dci_context(dci_cs_url=None, dci_login=None, dci_password=None,
         sys.exit(1)
     return DciContext(dci_cs_url, dci_login, dci_password,
                       user_agent=user_agent, max_retries=max_retries)
+
+
+class DciSignatureAuth(AuthBase):
+    """Signs the request for DCI API with signature authentication"""
+    def __init__(self, client_id, api_secret):
+        self.client_id = client_id
+        self.api_secret = api_secret
+
+    def __call__(self, r):
+        content_type = r.headers.get('Content-Type', '')
+        url_p = urlparse(r.url)
+
+        client_info = self.build_client_info()
+        sig = auth.sign(client_info, self.api_secret,
+                        r.method, content_type, url_p.path,
+                        url_p.query, r.body)
+        r.headers.update(self.build_headers(client_info, sig))
+        r.prepare_headers(r.headers)
+
+        return r
+
+    def build_client_info(self):
+        timestamp = datetime.utcnow()
+        return '%s/remoteci/%s' % (
+            timestamp.strftime('%Y-%m-%d %H:%M:%SZ'),
+            self.client_id
+        )
+
+    @staticmethod
+    def build_headers(client_info, sig):
+        return {
+            'DCI-Client-Info': client_info,
+            'DCI-Auth-Signature': sig,
+        }
+
+
+class DciSignatureContext(DciContextBase):
+    def __init__(self, dci_cs_url, client_id, api_secret, max_retries=0,
+                 user_agent=None):
+        super(DciSignatureContext, self).__init__(dci_cs_url, max_retries,
+                                                  user_agent)
+        self.session.auth = DciSignatureAuth(client_id, api_secret)
+
+
+def build_signature_context(dci_cs_url=None, dci_client_id=None,
+                            dci_api_secret=None,
+                            user_agent=None, max_retries=80):
+    dci_cs_url = dci_cs_url or os.environ.get('DCI_CS_URL', '')
+    dci_client_id = dci_client_id or os.environ.get('DCI_CLIENT_ID', '')
+    dci_api_secret = dci_api_secret or os.environ.get('DCI_API_SECRET', '')
+
+    if not dci_cs_url or not dci_client_id or not dci_api_secret:
+        print("Environment variables required: DCI_CS_URL, "
+              "DCI_CLIENT_ID, DCI_API_SECRET")
+        sys.exit(1)
+    return DciSignatureContext(dci_cs_url, dci_client_id, dci_api_secret,
+                               user_agent=user_agent, max_retries=max_retries)
---
+++ dciclient/v1/tests/test_signature.py
@@ -0,0 +1,66 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015-2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from datetime import datetime
+import requests
+
+from dciclient.v1 import auth
+import dciclient.v1.tests.shell_commands.utils as utils
+
+
+def make_blank_session(server):
+    s = requests.Session()
+    flask_adapter = utils.FlaskHTTPAdapter(server.test_client())
+    s.mount('http://dciserver.com', flask_adapter)
+    return s
+
+
+def test_get_job_with_no_auth_fails(server, job_id):
+    s = make_blank_session(server)
+    r = s.get('http://dciserver.com/api/v1/jobs/%s' % job_id)
+    assert r.status_code == 401
+
+
+def test_get_job_with_signature_succeeds(server, job_id, remoteci_id,
+                                         remoteci_api_secret):
+    timestamp = datetime.utcnow()
+    timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%SZ')
+    signature = auth.sign(
+        secret=remoteci_api_secret, http_verb='GET',
+        content_type='application/json',
+        timestamp=datetime.utcnow(),
+        url='/api/v1/jobs/%s' % job_id,
+        query_string='',
+        payload=''
+    )
+
+    sig_headers = {
+        'DCI-Client-Info': '%s/remoteci/%s' % (timestamp_str, remoteci_id),
+        'DCI-Auth-Signature': signature,
+        'Content-Type': 'application/json',
+    }
+
+    s = make_blank_session(server)
+    r = s.get('http://dciserver.com/api/v1/jobs/%s' % job_id,
+              headers=sig_headers)
+    assert r.status_code == 200
+
+
+def test_get_job_with_signature_context_succeeds(server, job_id, remoteci_id):
+    pass
+    # ctx = build_signature_context(dci_cs_url='', dci_client_id=remoteci_id)
+    # r = job.get(ctx,job_id)
+    # assert r.status_code == 201
