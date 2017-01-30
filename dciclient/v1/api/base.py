a890640200e7c0ebc628ba12c51b636d325f0cc0
Yanis Guenane <yguenane@redhat.com>
2017-01-30 14:11:12 +0100

    Enable the purge subcommand
    This commit aims to enable the purge subcommand feature.
    
    It will remove all the archived resources. The noop options allows one
    to list the resources that are about to be removed.
    
    Change-Id: Iecc7e0ce7c91463b3c748aa45aaca1040023d870

--- dciclient/v1/api/base.py
+++ dciclient/v1/api/base.py
@@ -106,3 +106,11 @@ def delete(context, resource, id, **kwargs):
 
     r = context.session.delete(uri, headers={'If-match': etag})
     return r
+
+
+def purge(context, resource, **kwargs):
+    """Purge resource type."""
+    data = utils.sanitize_kwargs(**kwargs)
+    uri = '%s/%s/purge' % (context.dci_cs_api, resource)
+    r = context.session.post(uri, data=json.dumps(data))
+    return r
