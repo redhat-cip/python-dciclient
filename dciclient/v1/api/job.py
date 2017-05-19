dc556e885fedd13a4ff8742b85b547cf5ce3599e
Gonéri Le Bouder <goneri@redhat.com>
2017-05-19 16:04:10 -0400

    job create: team_id not mandatory
    The team_id parameter is mandatory anymore. If the parameter is missing,
    the server user the team of the user.
    
    The commit also slightly adjust the header of job.py to avoid:
    H103  Header does not match Apache 2.0 License notice
    
    Depends-On: I6bd810ea8572dfec1bea41c1369ef0fd222b7cc1
    
    Change-Id: I1785bcddb1c1a606f6c2b9d5a44ee8e0e42ea87e

--- dciclient/v1/api/job.py
+++ dciclient/v1/api/job.py
@@ -1,12 +1,12 @@
 # -*- encoding: utf-8 -*-
 #
-# Copyright 2015-2016 Red Hat, Inc.
+# Copyright 2015-2017 Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -24,7 +24,7 @@ import json
 RESOURCE = 'jobs'
 
 
-def create(context, remoteci_id, team_id, jobdefinition_id=None,
+def create(context, remoteci_id, team_id=None, jobdefinition_id=None,
            components=None, comment=None):
     job = base.create(context, RESOURCE, remoteci_id=remoteci_id,
                       team_id=team_id, jobdefinition_id=jobdefinition_id,
