2911c209f4decb75c0127c9e17fee115d1f76cc7
Gonéri Le Bouder <goneri@redhat.com>
2017-06-01 15:22:31 -0400

    sample: list broken job results
    Loop on the existing job and find the ones are invalid:
    
    - older than 3 hours
    - dead after only 5 minutes
    - not finished after 10 hours
    
    Change-Id: I3d178f4a9e5c77e5b6de819779cac7f8acd8a878

---
+++ sample/list_broken_jobs.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from datetime import datetime
+from dciclient.v1.api import context as dci_context
+from dciclient.v1.api import job as dci_job
+from dciclient.v1.utils import str2date
+
+ctx = dci_context.build_dci_context()
+
+
+for j in dci_job.iter(ctx):
+    age = datetime.utcnow() - str2date(j['created_at'])
+
+    if age.total_seconds() < 3600 * 3:
+        # To young, maybe still running
+        continue
+
+    url = 'https://www.distributed-ci.io/#!/jobs/%s/results' % j['id']
+    jobstates = dci_job.list_jobstates(ctx, id=j['id'], ).json()['jobstates']
+    if len(jobstates) < 2:
+        print('no log: ' + url)
+        continue
+
+    end = str2date(jobstates[-1]['created_at'])
+    begin = str2date(jobstates[0]['created_at'])
+    duration = end - begin
+    if duration.total_seconds() < 60 * 5:
+        print('below 5 minutes: %s (duration: %s)' % (url, j['id'], duration))
+
+    if duration.total_seconds() > 3600 * 10:
+        print('above 10 hours: %s (duration: %s)' % (url, j['id'], duration))
