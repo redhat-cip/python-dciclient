053cfc75ba1ce2bad9724e732851932938285da8
Cedric LECOMTE <clecomte@redhat.com>
2017-09-05 12:57:20 +0200

    Add fingerprints call
    Change-Id: Ic30e9f74a8cdf7354d0ccc810459fb669d61bc68

---
+++ dciclient/v1/api/fingerprint.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015-2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from dciclient.v1.api import base
+
+
+RESOURCE = 'fingerprints'
+
+
+def create(context, name, state='active'):
+    return base.create(context, RESOURCE, name=name)
+
+
+def list(context, **kwargs):
+    return base.list(context, RESOURCE, **kwargs)
+
+
+def get(context, id, **kwargs):
+    return base.get(context, RESOURCE, id=id, **kwargs)
+
+
+def update(context, id, **kwargs):
+    return base.update(context, RESOURCE, id=id, **kwargs)
+
+
+def delete(context, id, **kwargs):
+    return base.delete(context, RESOURCE, id=id, **kwargs)
