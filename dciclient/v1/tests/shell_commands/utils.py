1518f38646fe9310cab05a09c9678cd7fe67747b
Yanis Guenane <yguenane@redhat.com>
2017-10-05 16:16:18 +0200

    Add REMOTECI role
    Change-Id: I58f8b1f1993252c09415dbd707a1380261d16329

--- dciclient/v1/tests/shell_commands/utils.py
+++ dciclient/v1/tests/shell_commands/utils.py
@@ -138,10 +138,17 @@ def provision(db_conn):
         'description': 'Regular User',
     }
 
+    feeder_role = {
+        'name': 'Feeder',
+        'label': 'FEEDER',
+        'description': 'A Feeder',
+    }
+
     super_admin_role_id = db_insert(models.ROLES, **super_admin_role)
     admin_role_id = db_insert(models.ROLES, **admin_role)
     user_role_id = db_insert(models.ROLES, **user_role)
     db_insert(models.ROLES, **product_owner_role)
+    db_insert(models.ROLES, **remoteci_role)
 
     # Create users
     db_insert(models.USERS, name='user', role_id=user_role_id,
