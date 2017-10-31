f446108c35be1449917ae2f63309db9f37ef6925
Yanis Guenane <yguenane@redhat.com>
2017-10-31 16:11:03 +0100

    Ensure dci_context_remoteci schedule jobs in tests
    Also fixes file creation: all "body" data passed to requests should be
    binary.
    
    Change-Id: I61e6c5432940f655ea1235095af597604b4a9a85

--- dciclient/v1/api/file.py
+++ dciclient/v1/api/file.py
@@ -54,12 +54,11 @@ def create(context, name, content=None, file_path=None, mime='text/plain',
     uri = '%s/%s' % (context.dci_cs_api, RESOURCE)
 
     if content:
-        if isinstance(content, bytes):
+        if not hasattr(content, 'read'):
+            if not isinstance(content, bytes):
+                content = content.encode('utf-8')
             content = io.BytesIO(content)
-        elif not hasattr(content, 'read'):
-            content = six.StringIO(content)
         return context.session.post(uri, headers=headers, data=content)
-
     else:
         if not os.path.exists(file_path):
             raise FileErrorException()
