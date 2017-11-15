8b2cfe6b9ac36a0bf05be7598391eb0f926b0c6b
Guillaume Vincent <gvincent@redhat.com>
2017-11-15 11:51:26 +0100

    Replace signature mechanism with HMAC mechanism from dciauth
    Change-Id: I9d1a0c7b234ac6ac97dd00ab73105bfb6add4daa
    Depends-On: Ifb2ae9a86f8b582767172a1716420dfb25036509

--- dciclient/v1/tests/test_signature.py
+++ dciclient/v1/tests/test_signature.py
@@ -14,11 +14,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from datetime import datetime
 import requests
 
+from dciauth import signature
 from dciclient.v1.api import job
-from dciclient.v1 import auth
 import dciclient.v1.tests.shell_commands.utils as utils
 
 
@@ -37,32 +36,23 @@ def test_get_job_with_no_auth_fails(server, job_id):
 
 def test_get_job_with_signature_succeeds(server, job_id, remoteci_id,
                                          remoteci_api_secret):
-    timestamp = datetime.utcnow()
-    timestamp_str = timestamp.strftime('%Y-%m-%d %H:%M:%SZ')
-    signature = auth.sign(
-        secret=remoteci_api_secret, http_verb='GET',
+    headers = signature.generate_headers_with_secret(
+        secret=remoteci_api_secret,
+        method='GET',
         content_type='application/json',
-        timestamp=datetime.utcnow(),
         url='/api/v1/jobs/%s' % job_id,
-        query_string='',
-        payload=None
-    )
-
-    sig_headers = {
-        'DCI-Client-Info': '%s/remoteci/%s' % (timestamp_str, remoteci_id),
-        'DCI-Auth-Signature': signature,
-        'Content-Type': 'application/json',
-    }
-
+        params={},
+        payload={})
+    headers['DCI-Client-Info'] = 'remoteci/%s' % remoteci_id
     s = make_blank_session(server)
     r = s.get('http://dciserver.com/api/v1/jobs/%s' % job_id,
-              headers=sig_headers)
+              headers=headers)
     assert r.status_code == 200
 
 
 def test_get_job_with_remoteci_context_succeeds(dci_context_remoteci, job_id):
-    context = dci_context_remoteci
-    r = job.get(context, job_id)
+    r = job.get(dci_context_remoteci, job_id)
+    print(r.text)
     assert r.status_code == 200
 
 
@@ -92,21 +82,3 @@ def test_server_url_with_trailing_slash(remoteci_id, remoteci_api_secret,
 
     r = job.get(dci_context, job_id)
     assert r.status_code == 200
-
-
-def test_get_payload_hash_binary(tmpdir):
-    f = tmpdir.join('test_get_payload_hash.bin')
-    content = u'lälä\x8b'.encode('utf-8')
-    f.write(content, 'wb')
-
-    stream = f.open('rb')
-    assert auth._get_payload_hash(stream) == auth._get_payload_hash(content)
-
-
-def test_get_payload_hash_empty(tmpdir):
-    f = tmpdir.join('test_get_payload_hash_empty.bin')
-    content = u''.encode('utf-8')
-    f.write(content, 'wb')
-
-    stream = f.open('rb')
-    assert auth._get_payload_hash(stream) == auth._get_payload_hash(content)
