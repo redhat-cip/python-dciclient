de632f210a92464514fd92956fe0f3f23b7b6d3c
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2017-12-04 16:23:19 +0100

    [tox]: deploy elasticsearch for the server
    Change-Id: I2a597d0124c45f4fc21da28c47c66a4e68b2e99a

--- run_tests.sh
+++ run_tests.sh
@@ -18,9 +18,17 @@
 
 set -x -e
 
-# if the database is already running we do not want to run this script
+# start PG only if it's not already running
 if [ -z "$DISABLE_DB_START" ]; then
-    pifpaf run postgresql -- py.test -v --cov-report html --cov dciclient $*
-else
-    PIFPAF_POSTGRESQL_URL='' py.test -v --cov-report html --cov dciclient $*
+    cp ../deps/dci-control-server/scripts/start_db.sh ./
+    sh ./start_db.sh
 fi
+
+# start ES only if it's not already running
+if [ -z "$DISABLE_ES_START" ]; then
+    cp ../deps/dci-control-server/scripts/start_es.sh ./
+    sh ./start_es.sh
+
+fi
+
+py.test -v --cov-report html --cov dciclient $*
