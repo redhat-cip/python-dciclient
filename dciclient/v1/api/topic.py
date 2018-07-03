9f853ba9e4535559ba8bb7dad1f99e2097610c4a
Yanis Guenane <yguenane@gmail.com>
2018-07-03 13:55:47 +0200

    Rename next_topic in next_topic_id
    Change-Id: I170ce98855370ecafc43f6f8ab009e376b45572b
    Depends-On: Icb27a657f437ebb5b1a981fe171ebd46874e1153

--- dciclient/v1/api/topic.py
+++ dciclient/v1/api/topic.py
@@ -21,10 +21,11 @@ RESOURCE = 'topics'
 
 
 def create(context, name, component_types, label=None, state='active',
-           product_id=None, next_topic=None, data=None):
+           product_id=None, next_topic_id=None, next_topic=None, data=None):
     return base.create(context, RESOURCE, name=name, label=label, state=state,
                        component_types=component_types, product_id=product_id,
-                       next_topic=next_topic, data=data)
+                       next_topic_id=next_topic_id, next_topic=next_topic,
+                       data=data)
 
 
 def list(context, **kwargs):
@@ -36,12 +37,12 @@ def get(context, id, **kwargs):
 
 
 def update(context, id, etag, name=None, component_types=None,
-           label=None, next_topic=None, state=None, product_id=None,
-           data=None):
+           label=None, next_topic_id=None, next_topic=None, state=None,
+           product_id=None, data=None):
     return base.update(context, RESOURCE, id=id, etag=etag, name=name,
                        component_types=component_types, label=label,
-                       next_topic=next_topic, state=state,
-                       product_id=product_id, data=data)
+                       next_topic_id=next_topic_id, state=state,
+                       product_id=product_id, next_topic=next_topic, data=data)
 
 
 def delete(context, id):
