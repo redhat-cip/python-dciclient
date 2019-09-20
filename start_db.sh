74b72e1628a8d557ff2bb8bdab3bb924408d4c9c
Guillaume Vincent <gvincent@redhat.com>
2019-09-20 14:43:23 +0200

    Use rh-postgresql96 instead of rh-postgresql94
    Change-Id: Ica605adf40e61f7aae18c3644f5914981fb4be36

--- start_db.sh
+++ start_db.sh
@@ -12,9 +12,7 @@ DCI_DB_DIR="$(cd "$(dirname "$0")/.." && pwd)/$DCI_DB_DIR"
 # if the database is already running we do not want to run this script
 [ ! -z "$DISABLE_DB_START" ] &&exit 0
 
-if [ -f /opt/rh/rh-postgresql94/enable ]; then
-    source /opt/rh/rh-postgresql94/enable
-fi
+source /opt/rh/rh-postgresql96/enable
 
 # checks if pg_ctl command exists
 type "pg_ctl"
