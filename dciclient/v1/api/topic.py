21941007844c36679b67d42c6b86132f5cf862a0
François Charlier <francois.charlier@redhat.com>
2019-10-22 10:52:36 +0200

    Fix export_control default values
    - create: default to False (restrictions will apply)
    - update: default to None (don't change the value unless explicitly
        passed)
    
    Change-Id: Ic24264d3dcb12b7282d2d730eb8d0f03aa1d14ca

--- dciclient/v1/api/topic.py
+++ dciclient/v1/api/topic.py
@@ -22,7 +22,7 @@ RESOURCE = 'topics'
 
 def create(context, name, component_types, label=None, state='active',
            product_id=None, next_topic_id=None, next_topic=None, data=None,
-           export_control=True):
+           export_control=False):
     return base.create(context, RESOURCE, name=name, label=label, state=state,
                        component_types=component_types, product_id=product_id,
                        next_topic_id=next_topic_id, next_topic=next_topic,
@@ -39,7 +39,7 @@ def get(context, id, **kwargs):
 
 def update(context, id, etag, name=None, component_types=None,
            label=None, next_topic_id=None, next_topic=None, state=None,
-           product_id=None, data=None, export_control=True):
+           product_id=None, data=None, export_control=None):
     return base.update(context, RESOURCE, id=id, etag=etag, name=name,
                        component_types=component_types, label=label,
                        next_topic_id=next_topic_id, state=state,
