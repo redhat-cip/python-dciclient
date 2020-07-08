e676108cdd645b4b840b4f5b46f461b5574a14be
ylamgarchal <yassine.lamgarchal@redhat.com>
2020-07-08 13:42:19 +0200

    test_components.py: temporary disable test_delete_tag
    this test will be re added after the change in the server
    side
    
    Change-Id: I810071f49eca46df67cc55f40db17d86bfe73cbe

--- tests/api/test_components.py
+++ tests/api/test_components.py
@@ -40,8 +40,7 @@ def test_add_tag(dci_context, component_id):
     res = component.add_tag(dci_context, component_id, "tag 1")
     assert res.status_code == 201
 
-
-def test_delete_tag(dci_context, component_id):
-    tag = component.add_tag(dci_context, component_id, "tag 1").json()["tag"]
-    res = component.delete_tag(dci_context, component_id, tag["id"])
-    assert res.status_code == 204
+# def test_delete_tag(dci_context, component_id):
+#    tag = component.add_tag(dci_context, component_id, "tag 1").json()["tag"]
+#    res = component.delete_tag(dci_context, component_id, tag["id"])
+#    assert res.status_code == 204
