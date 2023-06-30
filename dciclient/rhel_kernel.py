b095c2004317f0351507de8d64fce486382976ce
ylamgarchal <yassine.lamgarchal@redhat.com>
2023-06-30 15:30:31 +0200

    rhel_kernel.py: check remoteci connectivity
    Change-Id: I98b6dce26cc0c7741d37bce18f6277f9936116de

--- dciclient/rhel_kernel.py
+++ dciclient/rhel_kernel.py
@@ -17,6 +17,7 @@ import os
 import sys
 from argparse import ArgumentParser
 
+from dciclient.v1.api import identity
 from dciclient.v1.api import topic
 from dciclient.v1.api import product
 from dciclient.v1.shell_commands import context as dci_context
@@ -70,6 +71,18 @@ def get_topic_id_from_name(context, name):
     return topic_list["topics"][0]["id"]
 
 
+def check_remoteci_validity(context):
+    self = identity.get(context=context)
+    if self.status_code != 200:
+        print("""error from the api: %s
+please check the following:
+  - that your machine is correctly time synced
+  - that your remoteci credentials is correctly downloaded from the DCI dashboard
+if you didn't fixed this issue, please contact distributed-ci@redhat.com
+        """ % self.text)
+        sys.exit(1)
+
+
 def main():
     args = parse_arguments(sys.argv[1:], os.environ)
 
@@ -78,6 +91,8 @@ def main():
     if not context:
         print("No DCI credentials provided.")
         return 1
+
+    check_remoteci_validity(context)
 
     if args.topic_list:
         print_available_topics(context)
