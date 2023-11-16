53997d91f6c445e46538bc216331a62b7cf91d7e
Guillaume Vincent <gvincent@redhat.com>
2023-11-16 13:47:36 +0000

    Remove Dockerfile
    python dciclient container is replaced by dci tox container
    Removing the Dockerfile not needed
    
    Change-Id: If3b65c2a3a2f51fec435bc869b3a61bba4ea617e

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# TODO(guillaume) remove this when we will be able to run test with dci-control-server in build_rpm.sh
-sed -i 's#git+http://softwarefactory-project.io/r/dci-control-server#-e ../dci-control-server#g' /opt/python-dciclient/test-requirements.txt
-
-exec "$@"
